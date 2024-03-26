546338da7f4c567db48f66eb8bb29d8898ffd5b6
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-26 14:41:46 +0530

    Add login and logout functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,20 @@ const App = () => {
   const loginHandler = () => {
     dispatch(isAtuhencated(true));
   };
+
+  const logOutHandler = () => {
+    dispatch(isAtuhencated(false));
+  };
   return (
     <div className=" text-red-600">
       <h1>Hello From App</h1>
       <button onClick={loginHandler} className=" p-4 bg-black m-3 rounded-lg">
         Login
+      </button>
+
+      <button className=" p-4 m-3 bg-black rounded-lg" onClick={logOutHandler}>
+        {" "}
+        Logout
       </button>
     </div>
   );
